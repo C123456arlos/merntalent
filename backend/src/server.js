68123fffd8c4ef84b6818193fd6564144ad0ba49
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-04 10:16:59 -0400

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { ENV } from './lib/env.js'
+
+const app = express()
+
+console.log(ENV.PORT)
+app.get('/health', (req, res) => {
+    res.status(200).json({message:'api is up and running'})
+})
+app.listen(ENV.PORT, ()=>console.log('server running on port', ENV.PORT))
