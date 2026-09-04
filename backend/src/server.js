8b85e0f91726810823d6f4508a624aea3a935145
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-04 18:18:45 -0400

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,19 @@ import { ENV } from './lib/env.js'
 import path from 'path'
 import cors from 'cors'
 import { connectDB } from './lib/db.js'
-import {serve} from 'inngest/express'
+import { serve } from 'inngest/express'
+import {clerkMiddleware} from '@clerk/express'
 import { inngest, functions } from './lib/inngest.js'
+import chatRoutes from './routes/chatRoutes.js'
 const app = express()
 const __dirname = path.resolve()
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
-
+app.use(clerkMiddleware())
 app.use('/api/inngest', serve({client:inngest, functions}))
-
+app.use('/api/chat', chatRoutes)
 app.get('/health', (req, res) => {
     res.status(200).json({message:'api is up and running'})
-})
-app.get('/books', (req, res) => {
-    res.status(200).json({message:'this is the books endpoint'})
 })
 if (ENV.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
