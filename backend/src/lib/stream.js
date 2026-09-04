46aaef8cb05f6eeb6be9cba83cb7757c78ce8169
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-04 19:52:55 -0400

    first commit

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,10 +1,12 @@
 import { StreamChat } from 'stream-chat'
 import {ENV} from './env.js'
+import { StreamClient } from '@stream-io/node-sdk'
 const apiKey=ENV.STREAM_API_KEY
 const apiSecret = ENV.STREAM_API_SECRET
 if (!apiKey || !apiSecret) {
     console.error('stream api key or secret is missing')
 }
+export const streamClient=new StreamClient(apiKey, apiSecret)
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret)
 export const upsertStreamUser = async (userData) => {
     try {
